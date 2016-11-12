68a91c5207386ca8ac8cc043c1bf55fd22d6e806
JNO <novotny@fg.cz>
2016-11-12 21:18:26 +0100

    Cleaning and refactoring.

--- src/main/java/cz/novoj/generation/proxyGenerator/infrastructure/ProxyStateAccessor.java
+++ src/main/java/cz/novoj/generation/proxyGenerator/infrastructure/ProxyStateAccessor.java
@@ -1,16 +1,21 @@
 package cz.novoj.generation.proxyGenerator.infrastructure;
 
+import static cz.novoj.generation.proxyGenerator.infrastructure.MethodClassification.*;
 import static cz.novoj.generation.proxyGenerator.infrastructure.ReflectionUtils.isMethodDeclaredOn;
-
 
 public interface ProxyStateAccessor {
 
+	/**
+	 * Returns internal state of the proxy that is unique to each instance.
+	 * @return
+	 */
     Object getProxyState();
 
+	/** METHOD CONTRACT: Object getProxyState() **/
     static MethodClassification<ProxyStateAccessor, Void, Object> getProxyStateMethodInvoker() {
         return new MethodClassification<>(
         /* matcher */       method -> isMethodDeclaredOn(method, ProxyStateAccessor.class, "getProxyState"),
-        /* methodContext */ method -> null,
+        /* methodContext */ NO_CONTEXT,
         /* invocation */    (proxy, method, args, methodContext, proxyState) -> proxyState
         );
     }
